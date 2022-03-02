bb67d9ee3321c103b6108a91a2f3306dc894e140
Savannah <savannahw5859@gmail.com>
2022-03-02 15:34:58 -0500

    Adding logic for control singals

--- control.h
+++ control.h
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-using std::bitset;
 
 // Control signals for the processor
 struct control_t {
@@ -14,7 +13,7 @@ struct control_t {
     bool branch;             // 1 if branch
     bool mem_read;           // 1 if memory needs to be read
     unsigned mem_to_reg: 2;  // 00 if wirint ALU result (no mem access), 01 if memory needs to written to reg, 10 is JAL instruction
-    unsigned ALU_op : 2;     // 10 for R-type, 00 for LW/SW, 01 for BEQ/BNE, 11 for others
+    unsigned ALU_op : 2;     // 10 for R-type, 00 for LW/SW/JR, 01 for BEQ/BNE, 11 for others
     bool mem_write;          // 1 if needs to be written to memory
     unsigned ALU_src: 2;     // 00 if second operand is from reg_file, 01 if imm, 10 if unsigned imm
     bool reg_write;          // 1 if need to write back to reg file
@@ -22,6 +21,7 @@ struct control_t {
     bool jump_register;      // 1 if JR
     bool bne;                // 1 if BNE
     unsigned load_size : 2;  // 00 for Load Byte, 01 for Load Halfword, and 10 for Load Word
+    bool lui;                // 1 if lui
 
     
     void print() {      // Prints the generated contol signals
@@ -38,24 +38,116 @@ struct control_t {
         cout << "JUMP_REGISTER: " << reg_write << "\n";
         cout << "BNE: " << reg_write << "\n";
         cout << "LOAD_SIZE: " << reg_write << "\n";
+        cout << "LUI: " << lui << "\n";
     }
     // TODO:
     // Decode instructions into control signals
     void decode(uint32_t instruction) {
-        cout << "instruction " << instruction << "\n";
-        // std::string binary = std::bitset<32>(instruction).to_string();
-        
-        int j = 2;
-        string bits = "000000000000000000000000000000";
-        for(int i=32; i>0; i--){
-           if(instruction>(pow(j,(32-i))));
-            string ins = instruction%pow((int)j,(i));
-            bits[32-i] = ins;
-            instruction = instruction/2;
+        cout << "Instruction: " << "\n";
 
-       }
+        instruction = 156;
+        // std::cout << std::hex << 156 << "\n";
 
-       cout << bits;
+        // extract segment
+        int opcode = instruction >> 26;
+        int rs = instruction << 6 >> 27;
+        int rt = instruction << 11 >> 27;
+        int rd = instruction << 16 >> 27;
+        int shamt = instruction << 21 >> 27;
+        int funct = instruction << 26 >> 26;
+        int immediate = instruction << 16 >> 16;
+        int address = instruction << 6 >> 6;
+
+        // compare to existing opcodes (switch statement)
+        if (opcode == 0) { // R-type (JR and shift are special cases)
+            if (funct == 8) { // JR
+                jump_register = 1;
+                reg_write = 0;
+                reg_dest = 0;
+                ALU_op = 00;
+                jump = 0;
+            }
+            // else if (funct == 0) { // shift left logical
+
+            // }
+            // else if (funct == 2) { // shift right logical
+
+            // } 
+            else { // all other R-types
+                mem_read = 0;
+                mem_write = 0;
+                reg_dest = 1;
+                ALU_src = 0;
+                reg_write = 1;
+                ALU_op = 2;
+            }
+        } else if (opcode == 2) { // J
+            jump = 1;
+            jump_register = 0;
+        } else if (opcode == 3) { // JAL
+            jump = 1;
+            jump_register = 0;
+            mem_to_reg = 2;
+            lui = 0;
+        }
+        else { // I-type (branch, store, load, others)
+            if (opcode == 4) { //BEQ
+                branch = 1;
+                bne = 0;
+                ALU_src = 0;
+                ALU_op = 1; 
+                jump = 0;
+                jump_register = 0;
+            } else if (opcode == 5) { // BNE
+                branch = 0;
+                bne = 1;
+                ALU_src = 0;
+                ALU_op = 1; 
+                jump = 0;
+                jump_register = 0;
+            } else if (opcode == 40 | opcode == 41 | opcode == 43) { // stores
+                ALU_src = 1;
+                reg_write = 0;
+                mem_read = 0;
+                mem_write = 1;
+                branch = 0;
+                ALU_op = 0;
+            } else if (opcode == 35 | opcode == 37 | opcode == 36) { // loads
+                if (opcode == 35) { // load word
+                    ALU_src = 1;
+                    reg_write = 1;
+                    mem_read = 1;
+                    mem_write = 0;
+                    branch = 0;
+                    ALU_op = 0;
+                    reg_dest = 0;
+                    mem_to_reg = 1;
+                    load_size = 2;
+                } else if (opcode == 37) { // load halfword
+                    ALU_src = 2;
+                    reg_write = 1;
+                    mem_read = 1;
+                    mem_write = 0;
+                    branch = 0;
+                    ALU_op = 0;
+                    reg_dest = 0;
+                    mem_to_reg = 1;
+                    load_size = 1;
+                } else { // load byte
+                    ALU_src = 2;
+                    reg_write = 1;
+                    mem_read = 1;
+                    mem_write = 0;
+                    branch = 0;
+                    ALU_op = 0;
+                    reg_dest = 0;
+                    mem_to_reg = 1;
+                    load_size = 0;
+                }
+            }
+        }
+
+
     }
 
 };
